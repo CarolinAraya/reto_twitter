444639086d9972bce032cdaae1e101907fc40a80
carolinaraya <_carolina_@hotmail.cl>
2018-06-12 12:06:39 -0400

    funciona manipulano DOM pero debo mejorar css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
-document.getElementById('btn').addEventListener('click', (event)=>{
+document.getElementById('btn').addEventListener('click', () => {
   //event.preventDefault();
- let tweet = document.getElementById('textareaTweet').value;
- document.getElementById('tweetSent').innerText= tweet;
+  let tweet = document.getElementById('textareaTweet').value;
+  //creo div contenedor para el tweet enviado
+  const containerTweet = document.getElementById('tweetSent')
+  let tweetSent = document.createElement("div");
+  containerTweet.appendChild(tweetSent);
+
+  tweetSent.innerText = tweet;
 });
